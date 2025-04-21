79671b878c0f6d971e908738f50120202bb35c84
mariettepeutz <mariapeutz@gmail.com>
2025-04-21 15:36:58 -0700

    Switched to local backend and added .env config

--- taflab-heatmap-frontend/src/HeatmapWindow.js
+++ taflab-heatmap-frontend/src/HeatmapWindow.js
@@ -42,7 +42,7 @@ const HeatmapWindow = () => {
         }
       `;
       try {
-        const response = await fetch("http://64.181.236.184:5000/graphql/boat_data", {
+        const response = await fetch("http://127.0.0.1:5000/graphql/boat_data", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ query }),
